8bcf52ac6d208ff7e5d0b2ba81764b2497488b61
Nacho <ignacioportillo2004@gmail.com>
2025-10-15 16:56:33 -0300

    a

---
+++ js/Ignacio.js
@@ -0,0 +1,7 @@
+let productos = [
+{ nombre: "helado de chocolate", precio: 15, stock: 5, categoria: "helado", activo: true },
+{ nombre: "helado de pistacho", precio: 25, stock: 2, categoria: "helado", activo: true },
+{ nombre: "helado de dulce de leche", precio: 20, stock: 4, categoria: "helado", activo: false },
+{ nombre: "helado de frutilla ", precio: 10, stock: 6, categoria: "helado", activo: true },
+{ nombre: "helado de crema granizada", precio: 5, stock: 3, categoria: "helado", activo: true },
+];
